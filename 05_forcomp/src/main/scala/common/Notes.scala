67800abf74beb84007c7267a0ba3bc5deb508f38
Marcin Miszczyk <mpmiszczyk@gmail.com>
2017-09-30 17:31:13 +0200

    Added parameeters. But too many lt calls.

--- 05_forcomp/src/main/scala/common/Notes.scala
+++ 05_forcomp/src/main/scala/common/Notes.scala
@@ -2,23 +2,23 @@ package notes
 
 object Session {
 
-  def mergeSort (list: List[Int]): List[Int] = list match {
+  def mergeSort[T] (list: List[T])(lt: (T,T) => Boolean): List[T] = list match {
     case List() => list
     case List(i) => list
     case _ =>
       val middle: Int = list.length / 2
       val (first, second) = list splitAt (middle)
-      merge(mergeSort(first), mergeSort(second))
+      merge(mergeSort(first)(lt), mergeSort(second)(lt))(lt) // to many `lt`
   }
 
-  def merge(first: List[Int], second: List[Int]): List[Int] =
+  def merge[T](first: List[T], second: List[T])(lt: (T,T) => Boolean): List[T] =
     (first, second) match {
       case (Nil, second) => second
       case (first, Nil) => first
-      case (f :: fs, s :: ss) if (s < f) =>
-        s :: merge(first, ss)
+      case (f :: fs, s :: ss) if (lt(s, f)) =>
+        s :: (merge(first, ss)(lt))
       case (f :: fs, second) =>
-        f :: merge(fs, second)
+        f :: merge(fs, second)(lt)
     }
 
 }
